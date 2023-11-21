e76ff1b5d84f60f632801671d8fbfb82a059c488
Md. Sumon Islam <sumonst21@gmail.com>
2023-11-21 08:54:56 +0100

    Update get_percent_diff_from_values.php

--- get_percent_diff_from_values.php
+++ get_percent_diff_from_values.php
@@ -3,5 +3,5 @@ $current_value = 16.58;
 $total_value = 19.99;
 $discounted_value = $total_value - $current_value;
 $percent_diff = ($discounted_value/$total_value) * 100;
-//echo $percent_diff;
-echo round($percent_diff);
+echo "raw diff    : $percent_diff \n";
+echo 'rounded diff: ' . round($percent_diff);
